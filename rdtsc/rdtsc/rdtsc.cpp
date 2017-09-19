42e635a6ece5de2a7e8c6b2661710ac9cac7e7b2
ybkimdb <ybkimdb@gmail.com>
2017-09-19 13:43:15 +0900

    RDTSC -FIRST

---
+++ rdtsc/rdtsc/rdtsc.cpp
@@ -0,0 +1,42 @@
+// rdtsc.cpp
+// processor: x86, x64  
+#include <stdio.h>  
+#include "stdafx.h"
+#include <intrin.h>  
+#include<windows.h>
+
+#pragma intrinsic(__rdtsc) 
+
+int main()
+{
+	LARGE_INTEGER fr, li, oli;
+	if (!QueryPerformanceFrequency(&fr))
+		printf_s("QueryPerformanceFrequency failed!\n");
+
+	double usec = 0.0, freq = 0.0, qpc = 0.0, dfr, diff;
+	unsigned __int64 i, oi, del, ldel, ddel;
+	freq = 2500000;
+	dfr = (double)fr.QuadPart;
+	freq = dfr;
+	oi = 0;
+	oli.QuadPart = 0;
+	for (int j = 1; j < 100; j++) {
+		Sleep(1000);
+		QueryPerformanceCounter(&li);
+		i = __rdtsc();
+		del = i - oi;
+		//		del = del / 1024;
+		ldel = li.QuadPart - oli.QuadPart;
+		ldel = ldel * 1024;
+		qpc = (double(ldel) / dfr);
+		qpc = qpc / 1024;
+		usec = (del / freq) / 1024;
+		diff = (usec - qpc) * 1000000;
+		ddel = del - ldel;
+		printf_s("%I64d rdtscticks usec= %12.8f qpccount %I64d qpc[msec] %12.8f delt-time[usec] %+6.5f deltic= %I64d \n", del, usec, ldel, qpc, diff, ddel);
+		//	printf_s("%I64d query performance = %f\n",ldel, qpc);
+		oi = i;
+		oli.QuadPart = li.QuadPart;
+	}
+	getchar();
+}
